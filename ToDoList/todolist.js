21de76afaede08f932916de787bddb8338d55a3b
Delvin <delvin@ngauv.ch>
2023-06-16 11:09:59 +0200

    Endpoints GET und POST wurden erstellt

--- ToDoList/todolist.js
+++ ToDoList/todolist.js
@@ -17,31 +17,58 @@ app.use(
 //generierte Daten von ChatGPT
 const tasks = [
   {
+    id: 1,
     title: "Einkauf erledigen",
-    creationDate: new Date(),
+    creationDate: new Date().toLocaleDateString(),
     completionDate: null,
   },
   {
+    id: 2,
     title: "Geburtstagskarte schreiben",
-    creationDate: new Date(),
-    completionDate: new Date("2023-06-15"),
+    creationDate: new Date().toLocaleDateString(),
+    completionDate: new Date("2023-06-15").toLocaleDateString(),
   },
   {
+    id: 3,
     title: "Sport treiben",
-    creationDate: new Date(),
-    completionDate: new Date("2023-06-14"),
+    creationDate: new Date().toLocaleDateString(),
+    completionDate: new Date("2023-06-14").toLocaleDateString(),
   },
   {
+    id: 4,
     title: "Reise planen",
-    creationDate: new Date(),
-    completionDate: null
+    creationDate: new Date().toLocaleDateString(),
+    completionDate: null,
   },
   {
+    id: 5,
     title: "Buch lesen",
-    creationDate: new Date(),
-    completionDate: new Date("2023-06-20"),
+    creationDate: new Date().toLocaleDateString(),
+    completionDate: new Date("2023-06-20").toLocaleDateString(),
   },
 ];
+
+app.get("/tasks", (req, res) => {
+  res.status(200).contentType("application/json").json(tasks);
+});
+
+app.post("/tasks", (req, res) => {
+  //const newTask = req.body;
+
+  const newCompletionDate = req.body.completionDate;
+
+  const newTask = {
+    id: tasks.length + 1,
+    title: req.body.title,
+    creationDate: new Date().toLocaleDateString(),
+    completionDate: newCompletionDate || null,
+  };
+
+  tasks.push(newTask);
+  res.status(201).contentType("application/json").send(newTask);
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example App listening on port ${port}`);
